3f8a2ea9e3769fcfdb936ccc5c725e335c404e87
arctarus <arctarusnet@gmail.com>
2010-11-07 18:16:50 +0100

    improvements to problem 1 and README

--- problem1.rb
+++ problem1.rb
@@ -1,8 +1,16 @@
 #!/usr/bin/ruby -w
 
+# If we list all the natural numbers below 10 that are multiples of 
+# 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+#
+# Find the sum of all the multiples of 3 or 5 below 1000.
+#
+def problem1(n)
+	multiples = (0..n-1).to_a
+	multiples.reject! {|i| i % 3 != 0 and i % 5 != 0}
+	multiples.inject {|sum,i| sum + i}
+end
+
 print "enter a number: "
 n = gets.to_i
-multiples = (0..n-1).to_a
-multiples.reject! {|i| i % 3 != 0 and i % 5 != 0}
-sum = multiples.inject {|sum,i| sum + i}
-puts "the answer is #{sum}"
+puts "the answer is #{problem1(n)}"
